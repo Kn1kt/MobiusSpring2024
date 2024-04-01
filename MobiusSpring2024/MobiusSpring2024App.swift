eeb56ac93a925686e01280424f081a70db414cc1
Nikita Konashenko <nkonashenko@gmail.com>
2024-04-02 00:14:09 +0300

    Initial Commit

---
+++ MobiusSpring2024/MobiusSpring2024App.swift
@@ -0,0 +1,17 @@
+//
+//  MobiusSpring2024App.swift
+//  MobiusSpring2024
+//
+//  Created by Nikita Konashenko on 02.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MobiusSpring2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
